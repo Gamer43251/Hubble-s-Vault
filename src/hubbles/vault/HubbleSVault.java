d9331315a620ba66e635c1129a813ca4c7b8532c
Jordan Dreelan <37139811+Gamer43251@users.noreply.github.com>
2025-11-05 09:33:22 +0000

    Refactored For Proper Naming

--- src/hubbles/vault/HubbleSVault.java
+++ src/hubbles/vault/HubbleSVault.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package hubble.s.vault;
+package hubbles.vault;
 
 /**
  *
  * @author dreel
  */
-public class HubbleSVault {
+public class HubblesVault {
 
     /**
      * @param args the command line arguments
