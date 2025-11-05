3a32ccf6e923050cac2b090be525b7462e52985b
Jordan Dreelan <37139811+Gamer43251@users.noreply.github.com>
2025-11-05 10:38:17 +0000

    Began Implementation Of Main Menu

--- src/hubbles/vault/HubbleSVault.java
+++ src/hubbles/vault/HubbleSVault.java
@@ -4,17 +4,58 @@
  */
 package hubbles.vault;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author dreel
  */
 public class HubblesVault {
-
+    static Scanner sc = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean valid = false;
+        System.out.println("===============================");
+        System.out.println("Welcome To Hubble's Vault");
+        System.out.println("[1] Login");
+        System.out.println("[2] Create Account");
+        System.out.println("[3] Exit");
+        System.out.println("How Would You Like To Proceed (1/2/3):");
+        
+        while(!valid){
+           try{
+               int choice = sc.nextInt();
+        
+                switch(choice){
+                    case 1:
+                        login();
+                        valid = true;
+                    case 2:
+                        createAccount();
+                        valid = true;
+                    case 3:
+                        System.exit(0);
+                        valid = true;
+                }
+                
+           }catch(InputMismatchException e){
+               System.out.println("Please Enter A Valid Integer");
+               valid = false;
+           }
+        }
+        
+    }
+    
+    public static void login(){
+        System.out.println("Enter Your Username: ");
+        String username = sc.next();
+    }
+    
+    public static void createAccount(){
+        
     }
     
 }
